7344fb244c6809bb87b4277c70f77f779f545801
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-02 11:57:16 +0530

    Add counter functionality to index.html and app.js

--- javascript-basic-projects/02-counter/setup/app.js
+++ javascript-basic-projects/02-counter/setup/app.js
@@ -1,1 +1,28 @@
 // javascript 
+let count = 0;
+const value = document.querySelector("#value")
+const btn = document.querySelectorAll(".btn")
+
+btns.array.forEach((btn) => {
+    btn.addEventListener('click',(e)=>{
+        const style = e.currentTarget.classList;
+        if(style.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count=0;
+        }
+
+        if(count>0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count===0){
+            value.style.color ="#222";
+        }
+        value.textContent = count;
+    })
+});
