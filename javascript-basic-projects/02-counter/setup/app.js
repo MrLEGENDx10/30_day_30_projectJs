e2cc817cf0b8e6814d6203d755b07758b55d3187
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-02 12:03:52 +0530

    Fix typo in app.js and update variable name

--- javascript-basic-projects/02-counter/setup/app.js
+++ javascript-basic-projects/02-counter/setup/app.js
@@ -1,14 +1,14 @@
 // javascript 
 let count = 0;
 const value = document.querySelector("#value")
-const btn = document.querySelectorAll(".btn")
+const btns = document.querySelectorAll(".btn")
 
-btns.array.forEach((btn) => {
+btns.forEach((btn) => {
     btn.addEventListener('click',(e)=>{
         const style = e.currentTarget.classList;
         if(style.contains("decrease")){
             count--;
-        }else if(styles.contains("increase")){
+        }else if(style.contains("increase")){
             count++;
         }else{
             count=0;
